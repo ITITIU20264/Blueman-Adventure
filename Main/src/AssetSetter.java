ea0972337d6a275fbca2698a08049e010456cfa1
petertran410 <91869248+ITITIU20264@users.noreply.github.com>
2022-05-21 20:42:16 +0700

    ADD SOME OBJECTS

--- Main/src/AssetSetter.java
+++ Main/src/AssetSetter.java
@@ -6,16 +6,16 @@ public class AssetSetter{
     }
     public void setobject(){
         gp.obj[0] = new OBJ_KEY(gp);
-        gp.obj[0].worldX = 23 * gp.tileSize;
-        gp.obj[0].worldY = 10 * gp.tileSize;
+        gp.obj[0].worldX = 2 * gp.tileSize;
+        gp.obj[0].worldY = 7 * gp.tileSize;
 
         gp.obj[1] = new OBJ_DOOR(gp);
-        gp.obj[1].worldX = 23 * gp.tileSize;
-        gp.obj[1].worldY = 9 * gp.tileSize;
+        gp.obj[1].worldX = 4 * gp.tileSize;
+        gp.obj[1].worldY = 7 * gp.tileSize;
 
         gp.obj[2] = new OBJ_DOOR(gp);
-        gp.obj[2].worldX = 12 * gp.tileSize;
-        gp.obj[2].worldY = 22 * gp.tileSize;
+        gp.obj[2].worldX = 7 * gp.tileSize;
+        gp.obj[2].worldY = 8 * gp.tileSize;
 
         gp.obj[3] = new OBJ_KEY(gp);
         gp.obj[3].worldX = 12 * gp.tileSize;
